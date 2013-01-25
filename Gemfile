670e1d67b37e46a3957a6582e79cb274db319e3d
John Burwell <jburwell@basho.com>
2013-01-25 09:22:42 -0500

    - Adds a Vagrant configuration to create the VM to drive the   demonstration

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'http://rubygems.org'
+gem 'vagrant', '1.0.5'
+
---
+++ config/site.pp
@@ -0,0 +1,30 @@
+node default {
+
+  $app_home = "/vagrant"
+  $app_install_script = "${app_home}/install_demo.sh"
+  $app_config_file = "/opt/app/config.rb"
+
+  notify { "Building a ${::cluster_num_nodes} node Riak cluster on the ${::cluster_subnet}/24 network with an initial IP of ${::cluster_subnet}.${::cluster_initial_octet}." : }
+
+  package { "vim" :
+    ensure => latest,
+  }
+
+  exec { $app_install_script :
+    cwd => $app_home,
+    timeout => 0,
+  }
+
+  file { $app_config_file :
+    ensure  => present,
+    require => Exec[$app_install_script],
+    content => template("config.rb.erb"),
+  }
+
+  exec { "${app_home}/app/setup.rb" :
+    timeout => 0,
+    require => File[$app_config_file],
+  }
+
+}
+
